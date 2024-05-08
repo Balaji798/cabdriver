d39600b7f44fe708519bacbc8b2c3521a7c75fb9
Balaji798 <balajiy798@gmail.com>
2024-05-08 17:10:05 +0530

    new update

--- controller/cabdriver.js
+++ controller/cabdriver.js
@@ -427,13 +427,7 @@ exports.update_user_detail = async (req, res) => {
       });
     }
     if (
-      req.body.firstName &&
-      req.body.lastName &&
       req.body.total_experience &&
-      req.body.email &&
-      req.body.mobileNumber &&
-      req.body.pincode &&
-      req.body.dob &&
       req.body.vehicle_type
     ) {
       const data = await cabdriverModel.findOneAndUpdate(
@@ -443,13 +437,6 @@ exports.update_user_detail = async (req, res) => {
           vehicle_reg_img: await aws.uploadToS3(req.vehicle_reg_img.buffer),
           insurance_img: await aws.uploadToS3(req.insurance_img.buffer),
           road_tax_img: await aws.uploadToS3(req.road_tax_img.buffer),
-          firstName: req.body.fullName,
-          lastName: req.body.lastName,
-          email: req.body.email,
-          mobileNumber: req.body.mobileNumber,
-          pincode: req.body.pincode,
-          dob: req.body.dob,
-          address: req.body.address,
           driving_experience: {
             total_experience: req.body.total_experience,
             vehicle_type: req.body.vehicle_type,
