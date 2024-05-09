2b9221a5423c98e3ef4f2aab621db89d806d0934
Balaji798 <balajiy798@gmail.com>
2024-05-09 11:25:33 +0530

    new update

--- app.js
+++ app.js
@@ -40,15 +40,7 @@ app.use(function (req, res, next) {
   });
   
   // error handler
-  app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  
-    // render the error page
-    res.status(err.status || 500);
+  app.listen(8000, () => {
+    //connect();
+    console.log(`server started on port ${8000}`);
   });
-
-
-  module.exports = app;
